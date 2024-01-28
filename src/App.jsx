4c341da8eeaf5c582048362dc5dad2b7cbbe1fd3
AKarelina <annagjindj@gmail.com>
2024-01-27 23:53:35 -0800

    Add images

--- src/App.jsx
+++ src/App.jsx
@@ -20,10 +20,31 @@ function App() {
                 <div>
                     News
                 </div>
+                <div>
+                    Music
+                </div>
+                <div>
+                    Settings
+                </div>
             </nav>
 
             <div className="content">
-                Main Content
+                <img src="https://images.unsplash.com/photo-1471879832106-c7ab9e0cee23?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8fA%3D%3D"/>
+                <div>
+                    ava + description
+                </div>
+                <div>
+                    My posts
+                </div>
+                <div>
+                    New post
+                </div>
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
             </div>
             <footer className="footer">
                 social-network.com
